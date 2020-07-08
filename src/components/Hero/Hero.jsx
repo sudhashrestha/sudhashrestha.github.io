d1f83e51a694823e520199dbf19da5e0ab0c3df9
Sudha Shrestha <itsmesudha12@gmail.com>
2020-07-08 16:34:56 +0545

    Create landing page.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
+
 import PortfolioContext from '../../context/context';
+import HeroImg from '../Image/HeroImg';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
+  const { title, name, subtitle, cta, img } = hero;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -21,26 +23,35 @@ const Header = () => {
     }
   }, []);
 
+  const Wrapper = isDesktop ? Col : Container;
+
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
-            {subtitle || "I'm the Unknown Developer."}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
+        <Row className="hero-row">
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={300} distance="30px">
+            <HeroImg className="hero-icon" isMobile={isMobile} alt="icon" filename={img} />
+          </Fade>
+          <Wrapper>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+              <h1 className="hero-title">
+                {title || 'Hi, my name is'}{' '}
+                <span className="text-color-main">{name || 'Your Name'}</span>
+                <br />
+                {subtitle || "I'm the Unknown Developer."}
+              </h1>
+            </Fade>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <p className="hero-cta">
+                <span className="cta-btn cta-btn--hero">
+                  <Link to="about" smooth duration={1000}>
+                    {cta || 'Know more'}
+                  </Link>
+                </span>
+              </p>
+            </Fade>
+          </Wrapper>
+        </Row>
       </Container>
     </section>
   );
